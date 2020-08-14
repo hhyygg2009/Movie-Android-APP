6f56c75b9ca35a7e2afa172abd7e60677d6f7b80
hhyygg2009 <hhyygg2009@gmail.com>
2020-08-14 18:00:42 +0800

    addprojectfile

---
+++ app/src/main/java/com/yu/seemovie/SeeMovie.java
@@ -0,0 +1,14 @@
+package com.yu.seemovie;
+
+import android.app.Application;
+import org.xutils.x;
+
+public class SeeMovie extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        x.Ext.init(this);
+        x.Ext.setDebug(true);
+    }
+}
